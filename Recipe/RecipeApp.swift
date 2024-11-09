75c02621bd79870fa904c668d7c3907bbc6c291c
lashu333 <140723330+lashu333@users.noreply.github.com>
2024-11-09 14:22:20 +0400

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Lasha Tavberidze on 09.11.24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
