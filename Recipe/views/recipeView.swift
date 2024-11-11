d667d6d7a2b9edf6bd7ec39ed2fd638df70f2e91
lashu333 <140723330+lashu333@users.noreply.github.com>
2024-11-11 18:04:32 +0400

    RWVFT

--- Recipe/views/recipeView.swift
+++ Recipe/views/recipeView.swift
@@ -11,7 +11,20 @@ struct recipeView: View {
     let recipe: Recipe
     
     var body: some View {
+        VStack{
         Text("Hello,this is \(recipe.mainInfo.name) recipe")
+        Text("\(recipe.mainInfo.description)")
+        Text(recipe.mainInfo.author)
+        Image(recipe.mainInfo.imageName)
+            .resizable()
+            .scaledToFit()
+            .frame(width:100 , height:100)
+        Text(recipe.mainInfo.price.rawValue)
+        Text("\(recipe.mainInfo.time)")
+        ForEach (recipe.ingredients, id: \.name) { ingredient in
+            Text("\(ingredient.name)")
+        }
+    }
         }
     }
 
